e4a40b2becbf9551436247d8392a88f4f9f7b60f
William Spada <williamrspada02@gmail.com>
2024-07-06 14:06:49 -0300

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="bg-[#525E77] text-white text-center font-medium py-2">
+            Desenvolvido por Vinicius Costa, William Spada e Marco Aurelio
+        </footer>
+    );
+}
