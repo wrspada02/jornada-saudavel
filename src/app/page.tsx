ed52bd3eab4136c4f3fc5fd160b665aa653317dd
William Spada <williamrspada02@gmail.com>
2024-07-11 20:03:37 -0300

    feat: finish base html and css for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { PostCard } from "@/components/PostCard";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -17,6 +18,58 @@ export default function Home() {
             <PostCard />
             <PostCard />
           </ul>
+        </section>
+        <section>
+          <h2 className="font-bold text-xl text-center mb-14 mt-12">Conheça nossos especialistas</h2>
+          <article className="flex items-center justify-center gap-x-8 overflow-x-hidden">
+            <Image src={'/arrow-left.svg'} width={20} height={20} alt="Flecha de navegação à esquerda." />
+            <div className="max-w-[70%] overflow-hidden">
+              <ul className="flex items-center justify-center gap-9 flex-1">
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                    <figcaption className="font-medium text-sm text-center">Raquel</figcaption>
+                  </figure>
+                </li>
+              </ul>
+            </div>
+            <Image src={'/arrow-right.svg'} width={20} height={20} alt="Flecha de navegação à direita." />
+          </article>
+          <footer className="flex items-center justify-center gap-x-3 mt-5">
+            <div className="w-4 h-4 bg-black rounded-full"></div>
+            <div className="w-4 h-4 bg-black rounded-full"></div>
+            <div className="w-4 h-4 bg-black rounded-full"></div>
+          </footer>
         </section>
       </main>
       <Footer />
