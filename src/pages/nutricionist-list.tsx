646fa4b5a055716bc930fda6acfecc27c488d7ee
William Spada <williamrspada02@gmail.com>
2024-08-09 14:50:12 -0300

    feat: nutricionist list page

--- src/pages/nutricionist-list.tsx
+++ src/pages/nutricionist-list.tsx
@@ -1,35 +1,69 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { PostCard } from "@/components/PostCard";
+import Image from "next/image";
 import React from "react";
 
 export default function NutricionistList() {
     return (
         <>
             <Header />
-            <main className="px-3 py-6 bg-[#FDFDFD]">
-                <h1 className="font-bold text-xl mb-6">Publicações</h1>
-                <ul className="max-h-[78vh] overflow-auto">
-                    <li>
-                        <PostCard />
+            <main className="px-3 py-6 bg-[#FDFDFD] max-h-screen overflow-auto">
+                <h1 className="font-bold text-xl mb-6">Nutricionistas</h1>
+                <ul>
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
-                    <li>
-                        <PostCard />
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
-                    <li>
-                        <PostCard />
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
-                    <li>
-                        <PostCard />
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
-                    <li>
-                        <PostCard />
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
-                    <li>
-                        <PostCard />
-                    </li>
-                    <li>
-                        <PostCard />
+                    <li className="mt-7">
+                        <figure className="flex items-center gap-x-3">
+                            <Image src='/nutricionista_imagem.jpeg' width={70} height={70} className="rounded-full" alt="Nutricionista profile picture" />
+                            <figcaption>
+                                <h3 className="font-medium text-base">Raquel de Oliveira Santos</h3>
+                                <p className="mt-1 text-sm max-w-lg">Formada em nutrição em 2015, especializada em perca de peso pela USP, hoje Raquel contabiliza e auxilia mais de 100 clientes mensalmente.</p>
+                            </figcaption>
+                        </figure>
                     </li>
                 </ul>
             </main>
---
+++ src/pages/post-list.tsx
@@ -0,0 +1,39 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { PostCard } from "@/components/PostCard";
+import React from "react";
+
+export default function PostList() {
+    return (
+        <>
+            <Header />
+            <main className="px-3 py-6 bg-[#FDFDFD]">
+                <h1 className="font-bold text-xl mb-6">Publicações</h1>
+                <ul className="max-h-[78vh] overflow-auto">
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                    <li>
+                        <PostCard />
+                    </li>
+                </ul>
+            </main>
+            <Footer />
+        </>
+    );
+}
