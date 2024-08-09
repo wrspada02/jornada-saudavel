e747fd86e7d7e90bc545b980df40ed56dc3cbd33
William Spada <williamrspada02@gmail.com>
2024-08-09 14:17:47 -0300

    feat: open and close header modal on mobile screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,41 @@
+"use client";
 import Image from "next/image";
+import React from "react";
 
 export function Header() {
-    return (
-        <header className="flex items-center justify-between p-3 bg-[#C3E9D2]">
-            <Image src={'/logo.svg'} width={30} height={30} alt="Logo jornada saudável, uma imagem cartoon colorida de um copo de água com detalhes em branco e a água quase até a borda do copo, cheio, na cor azul." />
-            <Image src={'/button_hamburger.svg'} width={30} height={30} alt="Botão hamburguer para abrir o menu de opções do cabeçalho." />
-        </header>
-    );
+    const [isOpenHeaderMobile, setIsOpenHeaderMobile] = React.useState<boolean>(false);
+    return isOpenHeaderMobile ? (
+            <header className="w-screen h-screen bg-mobileHeader p-3 fixed top-0 left-0 z-10 animate-opacity-entrance">
+                <nav className="text-black">
+                    <ul className="flex items-end flex-col gap-5">
+                        <button onClick={() => {
+                            setIsOpenHeaderMobile(false);
+                        }}>
+                            <li className="font-semibold text-3xl cursor-pointer">X</li>
+                        </button>
+                        <li className="font-semibold text-xl cursor-pointer">Home</li>
+                        <li className="font-semibold text-xl cursor-pointer">Publicações</li>
+                        <li className="font-semibold text-xl cursor-pointer">Nutricionistas</li>
+                    </ul>
+                </nav>
+            </header>
+        ) : (
+            <header className="flex items-center justify-between p-3 tablet:p-5 desktop:p-6 bg-[#C3E9D2]">
+                <Image src={'/logo.svg'} width={30} height={30} alt="Logo jornada saudável, uma imagem cartoon colorida de um copo de água com detalhes em branco e a água quase até a borda do copo, cheio, na cor azul." />
+                <nav>
+                    <ul className="flex items-center gap-x-5">
+                        <li className="mobile:block hidden">
+                            <button onClick={() => {
+                                setIsOpenHeaderMobile(true);
+                            }}>
+                            <Image src={'/button_hamburger.svg'} width={30} height={30} alt="Botão hamburguer para abrir o menu de opções do cabeçalho." />
+                            </button>
+                        </li>
+                        <li className="mobile:hidden font-medium text-xl cursor-pointer">Home</li>
+                        <li className="mobile:hidden font-medium text-xl cursor-pointer">Publicações</li>
+                        <li className="mobile:hidden font-medium text-xl cursor-pointer">Nutricionistas</li>
+                    </ul>
+                </nav>
+            </header>
+        );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,6 @@ const config = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
     },
     extend: {
       colors: {
@@ -67,11 +64,41 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "opacity-entrance": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        "opacity-exit": {
+          from: { opacity: "1" },
+          to: { opacity: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "opacity-entrance": "opacity-entrance 0.5s ease-in-out",
+        "opacity-exit": "opacity-exit 0.5s ease-in-out",
       },
+      screens: {
+        "mobile": {
+          min: "0px",
+          max: "768px",
+        },
+        "tablet": {
+          min: "769px",
+          max: "1270px",
+        },
+        "desktop": {
+          min: "1271px",
+          max: "1920px",
+        },
+        "fullscreen": {
+          min: "1921px"
+        },
+      },
+      backgroundImage: {
+        mobileHeader: 'linear-gradient(180deg, rgba(253,253,253,1) 64%, rgba(195,233,210,1) 100%)',
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
