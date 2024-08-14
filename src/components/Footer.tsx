f42bec4001b56abe6d8a0b2fad9a7c866bb2c265
William Spada <williamrspada02@gmail.com>
2024-08-14 11:44:37 -0300

    chore: change footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="bg-[#525E77] text-white text-center font-medium p-2">
-            Desenvolvido por Vinicius Costa, William Spada e Marco Aurelio
+            Desenvolvido por Marco Aurélio, William Spada e Vinicius Costa
         </footer>
     );
 }
