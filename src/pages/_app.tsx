6d4eb990ca7951c8fd1bfab09ac5ce3d5a1e2aff
William Spada <williamrspada02@gmail.com>
2024-08-15 15:29:37 -0300

    chore: move middleware to src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // These styles apply to every route in the application
 import '@/app/globals.css';
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
  
 export default function App({ Component, pageProps }: AppProps) {
